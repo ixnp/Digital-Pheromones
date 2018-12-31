8bea6cff06a506369e6b6aeb8ab8b639f95c0fe4
ixnp <enprocopios@gmail.com>
2018-12-30 22:23:44 -0800

    made project more Responsive for mobile

--- scr/css/scripts/navigation.js
+++ scr/css/scripts/navigation.js
@@ -11,3 +11,15 @@ $('.menu li a').click(function(e) {
 
   e.preventDefault();
 });
+
+
+$("#hamburger").click(function(e) {
+  $(".menu").toggleClass("dropdown_menu");
+  $(".nav_large").toggleClass("dropdown");
+  $(".menu_wrapper_off").toggleClass("menu_wrapper");
+  $("#btn_nav").removeClass("btn");
+ 
+
+  e.preventDefault();
+});
+
