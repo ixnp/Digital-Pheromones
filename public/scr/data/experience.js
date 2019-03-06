7fddf3137197604c387dbb7afb017a86556905ff
ixnp <enprocopios@gmail.com>
2019-03-06 13:05:59 -0800

    fixed project fontsize for over 100px

--- public/scr/data/experience.js
+++ public/scr/data/experience.js
@@ -1,23 +1,23 @@
 'use strict';
 let cards = {
-  "experience": [
+  experience: [
     {
-      "title": "Development",
-      "icon": `icon-embed`,
-      "xlink":"icon-embed2",
-      "content": `<li>Unreal</li><li>Node.js</li><li>Python</li><li>IOS</li><li>C C#</li><li>Unity</li>`
+      title: "Development",
+      icon: `icon-embed`,
+      xlink: "icon-embed2",
+      content: `Unreal Node.js Python IOS C C# Unity`
     },
     {
-      "title": "Hardware",
-      "icon": `icon-powerplug`,
-      "xlink": "icon-power-cord",
-      "content": `<li>Unreal</li><li>Node.js</li><li>Python</li><li>IOS</li><li>C C#</li><li>Unity</li><`
+      title: "Hardware",
+      icon: `icon-powerplug`,
+      xlink: "icon-power-cord",
+      content: `Unreal Node.js Python IOS C C# Unity`
     },
     {
-      "title": "Design",
-      "icon": `icon-clipboard`,
-      "xlink": "icon-clipboard",
-      "content": `<li>Unreal</li><li>Node.js</li><li>Python</li><li>IOS</li><li>C C#</li><li>Unity</li>`
+      title: "Design",
+      icon: `icon-clipboard`,
+      xlink: "icon-clipboard",
+      content: `Unreal Node.js Python IOS C C# Unity`
     }
   ]
 };
