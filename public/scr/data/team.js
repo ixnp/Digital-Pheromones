c41fc239c0aa0ac424a2eac0556fb5c991de0638
ixnp <enprocopios@gmail.com>
2019-03-06 13:47:29 -0800

    typo image

--- public/scr/data/team.js
+++ public/scr/data/team.js
@@ -1,34 +1,35 @@
+'use strict';
 let team = {
-  "header":"A little about us",
-  "values": "Put a description of your buisness modle and core values here",
-  "teamMembers": [
+  'header':'A little about us',
+  'values': 'Put a description of your buisness modle and core values here',
+  'teamMembers': [
     {
-      "name": "Dr. Zachary Rubin",
-      "img": "scr/img/zac.png",
-  "link" : "",
-  "info" : "Zac likes bunnies"
-},
-    {
-      "name": "Zac",
-      "img": "scr/img/zac.png",
-      "link": "",
-      "info": "Zac likes bunnies"
+      'name': 'Dr. Zachary Rubin',
+      'img': 'scr/img/zac.png',
+      'link' : 'test',
+      'info' : 'Zac likes bunnies'
     },
     {
-      "name": "Zac",
-      "img": 'scr / img / Songbird_Logo_410x.png',
-      "link": "",
-      "info": "Zac likes bunnies"
+      'name': 'Zac',
+      'img': 'scr/img/zac.png',
+      'link': 'test',
+      'info': 'Zac likes bunnies'
     },
-]
-}
+    {
+      'name': 'Zac',
+      'img': 'scr/img/zac.png',
+      'link': 'test',
+      'info': 'Zac likes bunnies'
+    },
+  ]
+};
 
 $(document).ready(function() {
 
 
-  let teamTemplate = $("#team-template").html();
+  let teamTemplate = $('#team-template').html();
   let compiledteamTemplate = Handlebars.compile(teamTemplate);
   let setup = compiledteamTemplate(team);
 
-  $(".team").html(setup);
+  $('.team').html(setup);
 });
