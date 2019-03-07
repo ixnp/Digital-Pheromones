6b3b3d6d51fefd70e2a20768afc9151d8e9e6926
ixnp <enprocopios@gmail.com>
2019-03-07 11:49:30 -0800

    team content added

--- public/scr/data/team.js
+++ public/scr/data/team.js
@@ -1,26 +1,26 @@
 'use strict';
 let team = {
-  'header':'A little about us',
-  'values': 'Put a description of your buisness modle and core values here',
-  'teamMembers': [
+  header: "A little about us",
+  values: "Put a description of your buisness modle and core values here",
+  teamMembers: [
     {
-      'name': 'Dr. Zachary Rubin',
-      'imgurl': 'scr/img/zac.PNG',
-      'link' : 'test',
-      'info' : 'Zac likes bunnies'
+      name: "Dr. Zachary Rubin",
+      imgurl: "scr/img/zacrubin.jpg",
+      link: "https://www.linkedin.com/in/zakrubin/",
+      info: "Zac likes bunnies"
     },
     {
-      'name': 'Zac',
-      'imgurl': 'scr/img/zac.PNG',
-      'link': 'test',
-      'info': 'Zac likes bunnies'
+      name: "Haven Skinner",
+      imgurl: "scr/img/Skinner.jpg",
+      link: "https://www.linkedin.com/in/haven-skinner-7746ba19/detail/photo/",
+      info: "Zac likes bunnies"
     },
     {
-      'name': 'Zac',
-      'imgurl': 'scr/img/zac.PNG',
-      'link': 'test',
-      'info': 'Zac likesS bunnies'
-    },
+      name: "Yehonatan Koenig",
+      imgurl: "scr/img/yehonatan.jpg",
+      link: "https://www.linkedin.com/in/yehonatan-koenig-1b83b6/detail/photo/",
+      info: "Zac likesS bunnies"
+    }
   ]
 };
 
